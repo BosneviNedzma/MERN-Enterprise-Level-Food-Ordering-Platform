4b3e1640622b2b424e96dfb3325ead03eb416631
Nedzma Brkic <brkicnedzma@gmail.com>
2024-05-18 01:15:50 +0200

    Update user profile api

--- backend/src/routes/MyUserRoutes.ts
+++ backend/src/routes/MyUserRoutes.ts
@@ -1,9 +1,11 @@
+import { validateMyUserRequest } from "../middleware/validation";
+import { jwtCheck, jwtParse } from "../middleware/auth";
 import MyUserController from "../controllers/MyUserController";
 import express from "express";
-import { jwtCheck } from "../middleware/auth";
 
 const router = express.Router();
 
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
+router.put("/", jwtCheck, jwtParse, validateMyUserRequest, MyUserController.updateCurrentUser)
 
 export default router;
